ad33afc14eee643a43945e0026cc57d7839ff579
KLSewwanda <sewwandalakshitha28@gmail.com>
2026-04-02 18:59:24 +0530

    add dotenv and mongoose for environment configuration and MongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dns from 'dns';
+import dotenv from 'dotenv';
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+dotenv.config();
+
+
+const mongoDBURI = process.env.MONGO_URI
+mongoose.connect(mongoDBURI).then(
+    () => {
+        console.log("Connected mongoDB successfully")
+    }
+)
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-});
+})
