1d210b8c72bd67b9519952ab95b11660fe872509
KLSewwanda <sewwandalakshitha28@gmail.com>
2026-04-03 00:19:37 +0530

    add JWT authentication to login functionality ✏️

--- controllers/userContolles.js
+++ controllers/userContolles.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export async function createUser(req, res) {
     try {
@@ -38,10 +39,24 @@ export async function loginUser(req, res) {
             return res.status(400).json({ message: "User with this email does not exist" })
         }
         const isPasswordValid = await bcrypt.compare(password, user.password)
+
         
         if (isPasswordValid) {
 
-            return res.status(200).json({ message: "Login successful" })
+            const token = jwt.sign(
+                {
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    isAdmin: user.isAdmin,
+                    isBlocked: user.isBlocked,
+                    isEmailVerified: user.isEmailVerified,
+                    image: user.image
+                },
+                process.env.SECRET_KEY
+            )
+
+            return res.status(200).json({ message: "Login successful", token: token })
 
         } else {
 
